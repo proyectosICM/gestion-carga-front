0a343906e760a207ad550b7825028529d462a55f
Eduardo Aguilar <es123y123@gmail.com>
2024-05-16 18:20:18 -0500

    c

--- src/hooks/formatUtils.jsx
+++ src/hooks/formatUtils.jsx
@@ -1,15 +1,21 @@
 export const formatTiempoCarga = (tiempoCarga) => {
-    const hours = Math.floor(tiempoCarga / 3600);
-    const minutes = Math.floor((tiempoCarga % 3600) / 60);
-    const seconds = Math.floor(tiempoCarga % 60);
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-  };
-  
-  export const formatFecha = (fechaArray) => {
-    return `${fechaArray[0]}/${fechaArray[1]}/${fechaArray[2]}`;
-  };
-  
-  export const formatHora = (horaArray) => {
-    return `${horaArray[0]}:${horaArray[1]}:${horaArray[2]}`;
-  };
-  
+  if (!tiempoCarga || !Array.isArray(tiempoCarga)) {
+    return ""; // Retorna una cadena vacía si no se proporciona un valor válido
+  }
+
+  const hours = tiempoCarga[0] || 0;
+  const minutes = tiempoCarga[1] || 0;
+  const seconds = tiempoCarga[2] || 0;
+
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+};
+export const formatFecha = (fechaArray) => {
+  return `${fechaArray[0]}/${fechaArray[1]}/${fechaArray[2]}`;
+};
+
+export const formatHora = (horaArray) => {
+  const hora = horaArray[0];
+  const minutos = horaArray[1].toString().padStart(2, "0");
+  const segundos = horaArray[2].toString().padStart(2, "0");
+  return `${hora}:${minutos}:${segundos}`;
+};
